8b983031558f3bd6132a7008a3658c13d3e95e63
Corentin <c.ferry78@laposte.net>
2026-03-25 12:13:03 +0100

    feat: add method toString in tests and refact tests

--- src/test/java/com/magasin/MagasinTest.java
+++ src/test/java/com/magasin/MagasinTest.java
@@ -2,16 +2,21 @@ package com.magasin;
 
 import org.junit.jupiter.api.Test;
 
+import java.awt.event.ItemListener;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-class MagasinV1Test {
+class MagasinTest {
 
     @Test
     void foo() {
         Item[] items = new Item[] { new Item("foo", 0, 0) };
         MagasinV1 app = new MagasinV1(items);
+        Item[] itemsV2 = new Item[] { new Item("foo", 0, 0) };
+        MagasinV2 appV2 = new MagasinV2(itemsV2);
         app.updateQuality();
-        assertEquals("foo, -1, 0", app.items[0].toString());
+        appV2.updateQuality();
+        assertEquals(toString(itemsV2),toString(items));
     }
 
     @Test
@@ -19,8 +24,12 @@ class MagasinV1Test {
         Item[] items = new Item[] { new Item("Comté", 2, 4),
                                     new Item("Comté", 360, 10) };
         MagasinV1 app = new MagasinV1(items);
+        Item[] itemsV2 = new Item[] { new Item("Comté", 2, 4),
+                new Item("Comté", 360, 10) };
+        MagasinV2 appV2 = new MagasinV2(itemsV2);
         app.updateQuality();
-        assertEquals("Comté, 1, 5 Comté, 359, 11", app.items[0].toString()+' '+app.items[1].toString());
+        appV2.updateQuality();
+        assertEquals(toString(itemsV2),toString(items));
     }
 
     @Test
@@ -39,6 +48,14 @@ class MagasinV1Test {
         MagasinV1 app = new MagasinV1(items);
         app.updateQuality();
         assertEquals("Pass VIP Concert, 1, 7 Pass VIP Concert, 9, 50", app.items[0].toString()+' '+app.items[1].toString());
+    }
+
+    private String toString(Item[] items){
+        StringBuilder s = new StringBuilder();
+        for (Item item : items){
+            s.append(item.toString()+ "\n");
+        }
+        return s.toString();
     }
 
 }

713fb377ab86e681d301ed71ab9c2c6d29a6c588
Corentin <c.ferry78@laposte.net>
2026-03-25 11:40:30 +0100

    refactor: refact passVIPConcert test method with toString()

--- src/test/java/com/magasin/MagasinV1Test.java
+++ src/test/java/com/magasin/MagasinV1Test.java
@@ -38,13 +38,7 @@ class MagasinV1Test {
         Item[] items = new Item[] { new Item("Pass VIP Concert", 2, 4), new Item("Pass VIP Concert", 10, 50) };
         MagasinV1 app = new MagasinV1(items);
         app.updateQuality();
-        assertEquals("Pass VIP Concert", app.items[0].name);
-//        assertEquals(7, app.items[0].quality);
-//        assertEquals(1, app.items[0].sellIn);
-//        assertEquals(50, app.items[1].quality);
-//        assertEquals(9, app.items[1].sellIn);
-        assertEquals("Pass VIP Concert, 9, 50", app.items[1].toString());
-        assertEquals("Pass VIP Concert, 1, 7", app.items[0].toString());
+        assertEquals("Pass VIP Concert, 1, 7 Pass VIP Concert, 9, 50", app.items[0].toString()+' '+app.items[1].toString());
     }
 
 }

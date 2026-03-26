8d82e6f0bb0ac3d0dac8cee71edb1a62273e438c
Corentin FERRY <c.ferry78@laposte.net>
2026-03-26 10:26:26 +0100

    refactor: refactor MagasinV2

--- src/main/java/com/magasin/MagasinV2.java
+++ src/main/java/com/magasin/MagasinV2.java
@@ -9,59 +9,53 @@ class MagasinV2 {
 
     public void updateQuality() {
         for (Item item : items) {
-            if (!item.name.equals("Comté") && !item.name.equals("Pass VIP Concert")) {
-                if (item.quality > 0) {
-                    if (!item.name.equals("Kryptonite")) {
-                        item.quality = item.quality - 1;
-                    }
-                    if (item.name.equals("Pouvoirs Magiques")){
-                        item.quality = item.quality - 1;
-                    }
-                }
-            } else {
-                if (item.quality < 50) {
-                    item.quality = item.quality + 1;
-
-                    if (item.name.equals("Pass VIP Concert")) {
-                        if (item.sellIn < 11) {
-                            if (item.quality < 50) {
-                                item.quality = item.quality + 1;
-                            }
-                        }
-
-                        if (item.sellIn < 6) {
-                            if (item.quality < 50) {
-                                item.quality = item.quality + 1;
-                            }
-                        }
-                    }
-                }
-            }
-
-            if (!item.name.equals("Kryptonite")) {
-                item.sellIn = item.sellIn - 1;
-            }
-
-            if (item.sellIn < 0) {
-                if (!item.name.equals("Comté")) {
-                    if (!item.name.equals("Pass VIP Concert")) {
-                        if (item.quality > 0) {
-                            if (!item.name.equals("Kryptonite")) {
-                                item.quality = item.quality - 1;
-                            }
-                            if (item.name.equals("Pouvoirs Magiques")){
-                                item.quality = item.quality - 1;
-                            }
-                        }
-                    } else {
-                        item.quality = 0;
-                    }
-                } else {
-                    if (item.quality < 50) {
-                        item.quality = item.quality + 1;
-                    }
-                }
-            }
+            updateItem(item);
         }
     }
+    private void updateItem(Item item) {
+            switch (item.name) {
+                case "Kryptonite"      -> updateKryptonite(item);
+                case "Comté"           -> updateComte(item);
+                case "Pass VIP Concert"-> updateConcertPass(item);
+                case "Pouvoirs Magiques" ->updateMagicPowier(item);
+                default                -> updateNormalItem(item);
+            }
+    }
+
+    private void updateMagicPowier(Item item) {
+        item.sellIn--;
+        item.quality = Math.max(0, item.quality - (item.sellIn < 0 ? 4 : 2));
+    }
+
+    private void updateNormalItem(Item item) {
+            item.sellIn--;
+            item.quality = Math.max(0, item.quality - (item.sellIn < 0 ? 2 : 1));
+    }
+
+    private void updateKryptonite(Item item) {
+        // Aucune modifications
+    }
+
+    private void updateComte(Item item) {
+        item.sellIn--;
+        int increment = item.sellIn < 0 ? 2 : 1;
+        item.quality = Math.min(50, item.quality + increment);
+    }
+
+    private void updateConcertPass(Item item) {
+        item.sellIn--;
+
+        if (item.sellIn < 0) {
+            item.quality = 0;
+            return;
+        }
+
+        int increment = 1;
+        if (item.sellIn < 5)  increment = 3;
+        else if (item.sellIn < 10) increment = 2;
+
+        item.quality = Math.min(50, item.quality + increment);
+    }
 }
+
+
